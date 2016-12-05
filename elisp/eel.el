b8ea95e24d7d9becd092588a5e052e02d6fa371c
Eric Skoglund <eric@pagefault.se>
2016-12-05 23:25:16 +0100

    Changed some eel functions, added more packages

--- elisp/eel.el
+++ elisp/eel.el
@@ -1,6 +1,6 @@
 (provide 'eel)
 
-(defun eel-duplicate-line ()
+(defun eel/duplicate-line ()
   "Duplicates the current line below it"
   (interactive)
   (save-excursion
@@ -11,7 +11,7 @@
     (next-line 1)
     (yank)))
 
-(defun eel-copy-filename-to-killring ()
+(defun eel/copy-filename-to-killring ()
   "Copies the filename of the buffer into the clipboard"
   (interactive)
   (if (buffer-file-name)
@@ -19,3 +19,11 @@
 	(kill-new (buffer-file-name))
 	(message buffer-file-name))
     (message "Buffer has no file name associated with it")))
+
+(defun eel/switch-to-scratch ()
+  "Switches to the *scratch* buffer or goes back to
+the previous buffer that you visited"
+  (interactive)
+  (if (string-equal (buffer-name) "*scratch*")
+      (switch-to-buffer (other-buffer))
+    (switch-to-buffer "*scratch*")))
