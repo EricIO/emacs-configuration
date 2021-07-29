bd7db04e77fcc69b89e38c9b51a33f26f1854aa1
Eric Skoglund <eric@pagefault.se>
2021-07-29 19:37:50 +0200

    I cant even remember what I did but I bet it was goooood

--- elisp/eel.el
+++ elisp/eel.el
@@ -1,29 +1,41 @@
-(provide 'eel)
+;;; eel.el -- My personal little emacs functions that are mightely usefull.
+
+;;; Commentary:
+;;; Well this is a collection of usefull functions that I well...use.
+
+;;; Code:
 
 (defun eel/duplicate-line ()
-  "Duplicates the current line below it"
+  "Duplicates the current line below it."
   (interactive)
   (save-excursion
     (beginning-of-line)
     (kill-line)
     (yank)
     (open-line 1)
-    (next-line 1)
+    (forward-line 1)
     (yank)))
 
 (defun eel/copy-filename-to-killring ()
-  "Copies the filename of the buffer into the clipboard"
+  "Copies the filename of the buffer into the clipboard."
   (interactive)
   (if (buffer-file-name)
-      (progn 
+      (progn
 	(kill-new (buffer-file-name))
 	(message buffer-file-name))
     (message "Buffer has no file name associated with it")))
 
 (defun eel/switch-to-scratch ()
-  "Switches to the *scratch* buffer or goes back to
-the previous buffer that you visited"
+  "Switch to the *scratch* buffer or go back to the previous buffer that you visited."
   (interactive)
   (if (string-equal (buffer-name) "*scratch*")
       (switch-to-buffer (other-buffer))
     (switch-to-buffer "*scratch*")))
+
+(defun eel/insert-date-now ()
+  "Insert a date string formated like : 2021-03-13T13:00+01:00."
+  (interactive)
+  (insert (format-time-string "%Y-%m-%dT%H:%M:%S%z")))
+
+(provide 'eel)
+;;; eel.el ends here
